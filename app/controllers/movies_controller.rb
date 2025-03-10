8fb16a787914a48f201918fafc69f77f709fd7cf
yijordan <yij@uchicago.edu>
2025-03-10 04:08:03 +0000

    finalized form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,17 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    m.save
+
+    redirect_to("/movies")
+  end
 end
